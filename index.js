3d0d98192e4ebf95cd0f912054d26f1425089714
helloworldjs001 <helloworld.js001@gmail.com>
2024-05-08 21:35:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ app.use('/api/images', imageRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
+
+const startServer = ()=>{
+
+    
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+}
+
+startServer();
